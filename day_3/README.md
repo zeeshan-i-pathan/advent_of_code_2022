8b2bdc8a7c09cb9c9fb470fe9b7886647f5eb23c
Zeeshan Pathan <zeeshan.i.pathan@fwd.com>
2023-05-14 01:05:17 +0800

    Solution to Day 3 PART 1

---
+++ day_3/README.md
@@ -0,0 +1,3 @@
+*Day 3 Solution*
+
+For the Day 3 Solution I'm setting up the project as a lib with cargo new --lib so that I can Unit Test my solution before running against actual Input
--- day_3/src/lib.rs
+++ day_3/src/lib.rs
@@ -1,5 +1,49 @@
-pub fn part_1_score(input: &str) -> usize {
-    0
+trait Score {
+    fn score(self) -> u8;
+}
+
+impl Score for char {
+    fn score(self) -> u8 {
+        match self {
+            ('a'..='z') => self as u8 + 1 - b'a',
+            ('A'..='Z') => self as u8 + 27 - b'A',
+            _ => 0,
+        }
+    }
+}
+
+pub fn part_1_score(input: &str) -> u8 {
+    input
+        .lines()
+        .map(|line| {
+            let (bag_1, bag_2) = line.split_at(line.len() / 2);
+            bag_1
+                .chars()
+                .find(|item| bag_2.contains(*item))
+                .unwrap_or_default()
+                .score()
+        })
+        // .map(|bags| bags.0.chars().find(|item| bags.1.contains(*item)))
+        // .map(|mat| Point::try_from(mat.unwrap_or('a') as u8).unwrap().0)
+        .sum::<u8>()
+
+    // let mut score: usize = 0;
+    // for line in input.lines() {
+    //     let mid: usize = line.len() / 2;
+    //     let (bag_1, bag_2) = (&line[..mid], &line[mid..]);
+    //     for char in bag_1.chars() {
+    //         if bag_2.contains(char) {
+    //             let points = match char {
+    //                 ('a'..='z') => char as usize + 1 - 'a' as usize,
+    //                 ('A'..='Z') => char as usize + 27 - 'A' as usize,
+    //                 _ => 0,
+    //             };
+    //             score += points;
+    //             break;
+    //         }
+    //     }
+    // }
+    // score
 }
 
 #[cfg(test)]
